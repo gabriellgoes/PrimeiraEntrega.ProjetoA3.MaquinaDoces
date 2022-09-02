09e49757549f91c094b7912d0823dc6e201d45dc
Daniel Nicocelli <danicocelli@outlook.com>
2022-09-01 22:13:59 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraEntrega.ProjetoA3.MaquinaDoces/FrmMaquinaDoce.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace PrimeiraEntrega.ProjetoA3.MaquinaDoces
+{
+    partial class FrmMaquinaDoces
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUmReal = new System.Windows.Forms.Button();
+            this.btnDoisReais = new System.Windows.Forms.Button();
+            this.btnCincoReais = new System.Windows.Forms.Button();
+            this.lblRetorno = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnDoceC = new System.Windows.Forms.Button();
+            this.btnDoceB = new System.Windows.Forms.Button();
+            this.btnDoceA = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnUmReal
+            // 
+            this.btnUmReal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(244)))), ((int)(((byte)(232)))));
+            this.btnUmReal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUmReal.Location = new System.Drawing.Point(12, 262);
+            this.btnUmReal.Name = "btnUmReal";
+            this.btnUmReal.Size = new System.Drawing.Size(248, 95);
+            this.btnUmReal.TabIndex = 0;
+            this.btnUmReal.Text = "R$1,00";
+            this.btnUmReal.UseVisualStyleBackColor = false;
+            this.btnUmReal.Click += new System.EventHandler(this.btnUmReal_Click);
+            // 
+            // btnDoisReais
+            // 
+            this.btnDoisReais.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(208)))), ((int)(((byte)(249)))));
+            this.btnDoisReais.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoisReais.Location = new System.Drawing.Point(263, 262);
+            this.btnDoisReais.Name = "btnDoisReais";
+            this.btnDoisReais.Size = new System.Drawing.Size(248, 95);
+            this.btnDoisReais.TabIndex = 1;
+            this.btnDoisReais.Text = "R$2,00";
+            this.btnDoisReais.UseVisualStyleBackColor = false;
+            this.btnDoisReais.Click += new System.EventHandler(this.btnDoisReais_Click);
+            // 
+            // btnCincoReais
+            // 
+            this.btnCincoReais.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCincoReais.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCincoReais.Location = new System.Drawing.Point(517, 262);
+            this.btnCincoReais.Name = "btnCincoReais";
+            this.btnCincoReais.Size = new System.Drawing.Size(248, 95);
+            this.btnCincoReais.TabIndex = 2;
+            this.btnCincoReais.Text = "R$5,00";
+            this.btnCincoReais.UseVisualStyleBackColor = false;
+            this.btnCincoReais.Click += new System.EventHandler(this.btnCincoReais_Click);
+            // 
+            // lblRetorno
+            // 
+            this.lblRetorno.AutoSize = true;
+            this.lblRetorno.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRetorno.Location = new System.Drawing.Point(115, 158);
+            this.lblRetorno.Name = "lblRetorno";
+            this.lblRetorno.Size = new System.Drawing.Size(20, 24);
+            this.lblRetorno.TabIndex = 3;
+            this.lblRetorno.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bala";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(302, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Chiclete";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(567, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Chocolate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SALDO: R$";
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnComprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComprar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnComprar.Location = new System.Drawing.Point(391, 363);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(374, 95);
+            this.btnComprar.TabIndex = 8;
+            this.btnComprar.Text = "CONCLUIR COMPRA";
+            this.btnComprar.UseVisualStyleBackColor = false;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(11, 363);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(374, 95);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnDoceC
+            // 
+            this.btnDoceC.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnDoceC.Enabled = false;
+            this.btnDoceC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoceC.Location = new System.Drawing.Point(517, 219);
+            this.btnDoceC.Name = "btnDoceC";
+            this.btnDoceC.Size = new System.Drawing.Size(248, 37);
+            this.btnDoceC.TabIndex = 12;
+            this.btnDoceC.Text = "CHOCOLATE";
+            this.btnDoceC.UseVisualStyleBackColor = true;
+            this.btnDoceC.Click += new System.EventHandler(this.btnDoceC_Click);
+            // 
+            // btnDoceB
+            // 
+            this.btnDoceB.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnDoceB.Enabled = false;
+            this.btnDoceB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoceB.Location = new System.Drawing.Point(263, 219);
+            this.btnDoceB.Name = "btnDoceB";
+            this.btnDoceB.Size = new System.Drawing.Size(248, 37);
+            this.btnDoceB.TabIndex = 11;
+            this.btnDoceB.Text = "CHICLETE";
+            this.btnDoceB.UseVisualStyleBackColor = true;
+            this.btnDoceB.Click += new System.EventHandler(this.btnDoceB_Click);
+            // 
+            // btnDoceA
+            // 
+            this.btnDoceA.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnDoceA.Enabled = false;
+            this.btnDoceA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoceA.ForeColor = System.Drawing.Color.Black;
+            this.btnDoceA.Location = new System.Drawing.Point(12, 219);
+            this.btnDoceA.Name = "btnDoceA";
+            this.btnDoceA.Size = new System.Drawing.Size(248, 37);
+            this.btnDoceA.TabIndex = 10;
+            this.btnDoceA.Text = "BALA";
+            this.btnDoceA.UseVisualStyleBackColor = true;
+            this.btnDoceA.Click += new System.EventHandler(this.btnDoceA_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(754, 82);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Produtos";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(668, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 24);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "R$8,00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(377, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "R$7,00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(49, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "R$6,00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(13, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(375, 24);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Olá, bem-vindo(a) a Fábrica de Chocolates!";
+            // 
+            // FrmMaquinaDoces
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(779, 466);
+            this.Controls.Add(this.lblRetorno);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnDoceC);
+            this.Controls.Add(this.btnDoceB);
+            this.Controls.Add(this.btnDoceA);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCincoReais);
+            this.Controls.Add(this.btnDoisReais);
+            this.Controls.Add(this.btnUmReal);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMaquinaDoces";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Máquina de Doces";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUmReal;
+        private System.Windows.Forms.Button btnDoisReais;
+        private System.Windows.Forms.Button btnCincoReais;
+        private System.Windows.Forms.Label lblRetorno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnDoceC;
+        private System.Windows.Forms.Button btnDoceB;
+        private System.Windows.Forms.Button btnDoceA;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ PrimeiraEntrega.ProjetoA3.MaquinaDoces/FrmMaquinaDoce.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiraEntrega.ProjetoA3.MaquinaDoces
+{
+    public partial class FrmMaquinaDoces : Form
+    {
+        public FrmMaquinaDoces()
+        {
+            InitializeComponent();
+        }
+
+        public struct Doce 
+        {
+            public bool DoceA { get; set; }
+
+            public bool DoceB { get; set; }
+
+            public bool DoceC { get; set; }
+
+            //public Decimal? ValorDoceA { get; set; }
+
+            //public Decimal? ValorDoceB { get; set; }
+
+            //public Decimal? ValorDoceC { get; set; }
+        }
+
+
+        private void btnUmReal_Click(object sender, EventArgs e)
+        {
+            decimal valor = 1;
+            var doce = new Doce();
+            CalculaDoces(valor, doce);
+        }
+
+        private void btnDoisReais_Click(object sender, EventArgs e)
+        {
+            decimal valor = 2;
+
+            var doce = new Doce();
+            CalculaDoces(valor, doce);
+        }
+
+        private void btnCincoReais_Click(object sender, EventArgs e)
+        {
+            decimal valor = 5;
+            var doce = new Doce();
+            CalculaDoces(valor, doce);
+        }
+        
+        public void CalculaDoces(decimal valor, Doce doce)
+        {
+            decimal valorExistente = Convert.ToDecimal(lblRetorno.Text);
+            decimal total = 0;
+            if (doce.DoceA)
+            {
+                total = valorExistente - 6;
+                comprouDoce = true;
+            }
+
+            else if (doce.DoceB)
+            {
+                total = valorExistente - 7;
+                comprouDoce = true;
+            }
+            else if (doce.DoceC)
+            {
+                total = valorExistente - 8;
+                comprouDoce = true;
+            }
+            else total = valor + valorExistente;
+
+            lblRetorno.Text = total.ToString();
+
+            AtivaDoce();
+        }
+        bool comprouDoce = false;
+        public void AtivaDoce()
+        {
+            decimal total = Convert.ToDecimal(lblRetorno.Text);
+            
+            if(total == 6)
+            {
+                btnDoceA.BackColor = Color.LightGreen;
+                btnDoceA.Enabled = true;
+
+            }
+            else if(total == 7)
+            {
+                btnDoceA.BackColor = Color.LightGreen;
+                btnDoceB.BackColor = Color.LightGreen;
+                btnDoceA.Enabled = true;
+                btnDoceB.Enabled = true;
+            }
+            else if(total >= 8)
+            {
+                btnDoceA.BackColor = Color.LightGreen;
+                btnDoceB.BackColor = Color.LightGreen;
+                btnDoceC.BackColor = Color.LightGreen;
+                btnDoceA.Enabled = true;
+                btnDoceB.Enabled = true;
+                btnDoceC.Enabled = true;
+            }
+            else
+            {
+                ResetarCampos();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            if(lblRetorno.Text.Equals("0"))
+            {
+                MessageBox.Show("Nenhuma operação foi executada.", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("O total devolvido será de: R$" + lblRetorno.Text + ",00", 
+                    "COMPRA CANCELADA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                ResetarCampos();
+            }
+
+            lblRetorno.Text = "0";
+
+        }
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+            string valorTotal = lblRetorno.Text;
+
+
+            if (!comprouDoce)
+            {
+                MessageBox.Show("Nenhuma operação foi executada.", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Doces comprados! O seu troco será de: R$" + lblRetorno.Text + ",00",
+                    "COMPRA CONCLUÍDA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                ResetarCampos();
+            }
+
+            lblRetorno.Text = "0";
+
+            ResetarCampos();
+        }
+        
+        private void ResetarCampos()
+        {
+            btnDoceA.UseVisualStyleBackColor = true;
+            btnDoceB.UseVisualStyleBackColor = true;
+            btnDoceC.UseVisualStyleBackColor = true;
+
+            btnDoceA.Enabled = false;
+            btnDoceB.Enabled = false;
+            btnDoceC.Enabled = false;
+        }
+
+        private void btnDoceA_Click(object sender, EventArgs e)
+        {
+            decimal valorRecebido = Convert.ToDecimal(lblRetorno.Text);
+            var doce = new Doce();
+            doce.DoceA = true;
+            CalculaDoces(valorRecebido, doce);
+        }
+
+        private void btnDoceB_Click(object sender, EventArgs e)
+        {
+            decimal valorRecebido = Convert.ToDecimal(lblRetorno.Text);
+            var doce = new Doce();
+            doce.DoceB = true;
+            CalculaDoces(valorRecebido, doce);
+        }
+
+        private void btnDoceC_Click(object sender, EventArgs e)
+        {
+            decimal valorRecebido = Convert.ToDecimal(lblRetorno.Text);
+            var doce = new Doce();
+            doce.DoceC = true;
+            CalculaDoces(valorRecebido, doce);
+        }
+    }
+}
